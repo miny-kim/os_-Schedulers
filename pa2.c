35f5ce394c9ecc96afaa64e4f753269b6737da62
201720580 <min06814@ajou.ac.kr>
2019-11-04 17:10:18 +0000

    sjf done

--- pa2.c
+++ pa2.c
@@ -226,7 +226,6 @@ static struct process *sjf_schedule(void)
     }//next 결정해서 뽑기
 
     if(current->age < current->lifespan){
-        printf("11111111111\n");
         return current;}
 
 pick_next:
@@ -234,6 +233,7 @@ pick_next:
 if(!list_empty(&readyqueue)){
     pnext = list_first_entry(&readyqueue, struct process, list);
     endp = list_last_entry(&readyqueue, struct process, list);
+    printf("endp->pid : %d\n", endp->pid);
     next = pnext;
    // max = pnext->prio;
    // next = pnext;
@@ -241,16 +241,15 @@ if(!list_empty(&readyqueue)){
 
     while(pnext){
        
-        printf("for debugging(pid, lifespan) : %d, %d\n", pnext->pid, pnext->lifespan);
-
         if(min>pnext->lifespan){
             min = pnext->lifespan;
             next = pnext;
-            printf("max : %d\n", min);
         }
-        pnext = list_next_entry(pnext, list);
-        if(pnext->pid == 0)
+        if(pnext->pid == endp->pid)
             break;
+        pnext = list_next_entry(pnext, list);
+        //if(pnext->pid == 0)
+          //  break;
     }
 
     list_del_init(&next->list);
@@ -272,11 +271,63 @@ struct scheduler sjf_scheduler = {
 /***********************************************************************
  * SRTF scheduler
  ***********************************************************************/
+
+static struct process * srtf_schedule(void)
+{
+    struct process *next = NULL;
+    struct process *pnext = NULL;
+    struct process *endp = NULL;
+    int min = 100;
+    int remain = 0;
+   //현재 상태가 wait일때
+    if(!current || current->status == PROCESS_WAIT){
+        goto pick_next;
+    }//next 결정해서 뽑기
+
+    if(current->age < current->lifespan){
+        list_add_tail(&current->list, &readyqueue);
+        goto pick_next;
+    }
+
+pick_next:
+    
+if(!list_empty(&readyqueue)){
+    pnext = list_first_entry(&readyqueue, struct process, list);
+    endp = list_last_entry(&readyqueue, struct process, list);
+   // printf("endp->pid : %d\n", endp->pid);
+    next = pnext;
+   // max = pnext->prio;
+   // next = pnext;
+   // pnext = list_next_entry(pnext, list);
+
+    while(pnext){
+       remain = (pnext->lifespan)-(pnext->age);
+       printf("pid,remain : %d, %d\n", pnext->pid, remain);
+
+        if(min>remain){
+            min = remain;
+            next = pnext;
+        }
+        if(pnext->pid == endp->pid)
+            break;
+        pnext = list_next_entry(pnext, list);
+        //if(pnext->pid == 0)
+          //  break;
+    }
+
+    list_del_init(&next->list);
+}
+	return next;
+
+
+}
+
 struct scheduler srtf_scheduler = {
 	.name = "Shortest Remaining Time First",
 	.acquire = fcfs_acquire, /* Use the default FCFS acquire() */
 	.release = fcfs_release, /* Use the default FCFS release() */
-	/* You need to check the newly created processes to implement SRTF.
+	.schedule = srtf_schedule,
+    /* You need to check the newly created processes to implement SRTF.
 	 * Use @forked() callback to mark newly created processes */
 	/* Obviously, you should implement srtf_schedule() and attach it here */
 };
@@ -285,19 +336,189 @@ struct scheduler srtf_scheduler = {
 /***********************************************************************
  * Round-robin scheduler
  ***********************************************************************/
+static struct process * rr_schedule(void)
+{
+    struct process *next = NULL;
+    struct process *endp = NULL;
+
+   //현재 상태가 wait일때
+    if(!current || current->status == PROCESS_WAIT){
+        goto pick_next;
+    }//next 결정해서 뽑기
+
+    //quantum 적용하기
+    if(current->age < current->lifespan){
+        list_add_tail(&current->list , &readyqueue);
+        goto pick_next;
+    }
+
+pick_next:
+	if (!list_empty(&readyqueue)) {
+		/**
+		 * If the ready queue is not empty, pick the first process
+		 * in the ready queue
+		 */
+		next = list_first_entry(&readyqueue, struct process, list);
+
+		/**
+		 * Detach the process from the ready queue. Note we use list_del_init()
+		 * instead of list_del() to maintain the list head tidy. Otherwise,
+		 * the framework will complain (assert) on process exit.
+		 */
+		list_del_init(&next->list);
+	}
+
+	/* Return the next process to run */
+	return next;
+
+}
+
 struct scheduler rr_scheduler = {
 	.name = "Round-Robin",
 	.acquire = fcfs_acquire, /* Use the default FCFS acquire() */
 	.release = fcfs_release, /* Use the default FCFS release() */
-	/* Obviously, you should implement rr_schedule() and attach it here */
+    .schedule = rr_schedule,	/* Obviously, you should implement rr_schedule() and attach it here */
 };
 
 
 /***********************************************************************
  * Priority scheduler
  ***********************************************************************/
+
+bool prio_acquire(int resource_id)
+{
+	struct resource *r = resources + resource_id;
+
+	if (!r->owner) {
+		/* This resource is not owned by any one. Take it! */
+		r->owner = current;
+		return true;
+	}
+
+	/* OK, this resource is taken by @r->owner. */
+
+	/* Update the current process state */
+	current->status = PROCESS_WAIT;
+
+	/* And append current to waitqueue */
+	list_add_tail(&current->list, &r->waitqueue);
+
+	/**
+	 * And return false to indicate the resource is not available.
+	 * The scheduler framework will soon call schedule() function to
+	 * schedule out current and to pick the next process to run.
+	 */
+	return false;
+}
+
+void prio_release(int resource_id)
+{
+	struct resource *r = resources + resource_id;
+
+	/* Ensure that the owner process is releasing the resource */
+	assert(r->owner == current);
+   // printf("owner : %d\n", r->owner->pid);
+	/* Un-own this resource */
+	r->owner = NULL;
+
+
+	/* Let's wake up ONE waiter (if exists) that came first */
+	if (!list_empty(&r->waitqueue)) {
+		struct process *waiter =
+				list_first_entry(&r->waitqueue, struct process, list);
+        struct process * pwaiter = waiter;
+        struct process * endp = list_last_entry(&r->waitqueue, struct process, list);
+     //   printf("endp->pid : %d\n", endp->pid);
+        int max = 0;
+        
+        while(pwaiter){
+           // printf("first : %d\n", pwaiter->pid);
+            if(max<pwaiter->prio){
+                max = pwaiter->prio;
+                waiter = pwaiter;
+             //   printf("final : %d\n", waiter->pid);
+            }
+
+            if(pwaiter->pid == endp->pid)
+                break;
+            pwaiter = list_next_entry(pwaiter, list);
+            // printf("inside while (pwaiter->pid):%d\n", pwaiter->pid); 
+        }
+       // printf("outside while (waiter->pid) : %d\n", waiter->pid);
+		/**
+		 * Ensure the waiter  is in the wait status
+		 */
+		assert(waiter->status == PROCESS_WAIT);
+
+		/**
+		 * Take out the waiter from the waiting queue. Note we use
+		 * list_del_init() over list_del() to maintain the list head tidy
+		 * (otherwise, the framework will complain on the list head
+		 * when the process exits).
+		 */
+		list_del_init(&waiter->list);
+
+		/* Update the process status */
+		waiter->status = PROCESS_READY;
+
+		/**
+		 * Put the waiter process into ready queue. The framework will
+		 * do the rest.
+		 */
+		list_add_tail(&waiter->list, &readyqueue);
+	}
+}
+
+static struct process * prio_schedule(void)
+{
+    struct process * next = NULL;
+    struct process * pnext = NULL;
+    struct process * endp = NULL;
+    int max = 0;
+
+    if(!current || current->status == PROCESS_WAIT){
+        goto pick_next;
+    }
+
+    if(current->age < current->lifespan){
+        list_add_tail(&current->list, &readyqueue);
+        goto pick_next;
+    }
+
+pick_next:
+
+    if(!list_empty(&readyqueue)){
+        pnext = list_first_entry(&readyqueue, struct process, list);
+        endp = list_last_entry(&readyqueue, struct process, list);
+        next = pnext;
+   // max = pnext->prio;
+   // next = pnext;
+   // pnext = list_next_entry(pnext, list);
+
+        while(pnext){
+       
+            if(max<pnext->prio){
+                max = pnext->prio;
+                next = pnext;
+            }
+            if(pnext->pid == endp->pid)
+                break;
+            pnext = list_next_entry(pnext, list);
+            //if(pnext->pid == 0)
+              //  break;
+        }
+
+        list_del_init(&next->list);
+    }
+	return next;
+
+}
+
 struct scheduler prio_scheduler = {
 	.name = "Priority",
+    .acquire = prio_acquire,
+    .release = prio_release,
+    .schedule = prio_schedule,
 	/**
 	 * Implement your own acqure/release function to make priority
 	 * scheduler correct.
